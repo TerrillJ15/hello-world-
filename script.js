7807d3e73c2e3e50f10c19de1cfe9214f8757e8e
JESSICA <jessicalookenott15@gmail.com>
2023-06-26 12:19:23 -0400

    Added message with the like button

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ const element = React.createElement(
 
 ReactDOM.render(element, document.getElementById('root'));
 ReactDOM.render(element, document.getElementById('root2'));
+
+document
+  .querySelectorAll('.button')
+  // use querySelectorAll to select all three spans with class "button"
+  .forEach(domNode => {
+    // use forEach to loop through each of the span DOM node
+    // retrieve the message ID from the data attribute through the dataset property
+    const messageId = parseInt(domNode.dataset.messageId);
+    // create and render a React element for a button and attach an onClick event handler, it will log the messageID above
+    ReactDOM.render(
+      React.createElement(
+        'button',
+        {
+          onClick: () => {
+            console.log(`liked message: ${messageId}`);
+          },
+        },
+        'Like',
+      ),
+      domNode,
+    );
+  });
